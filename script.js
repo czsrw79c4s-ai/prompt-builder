dbd4b71c32d132940c20f90aeb761e78af126259
Tsuyukugun <czsrw79c4s@privaterelay.appleid.com>
2026-07-26 09:06:51 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const girlButton = document.getElementById("girlButton");
+const promptBox = document.getElementById("promptBox");
+
+girlButton.addEventListener("click", () => {
+
+    promptBox.textContent = "girl";
+
+});
